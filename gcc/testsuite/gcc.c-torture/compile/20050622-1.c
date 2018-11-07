6810bfe4dac87bd34e568c9c2637a5c01fb1cc03
Tatyana Krasnukha <tatyana@synopsys.com>
2018-11-07 22:18:05 +0300

    Prevent Clang from going out of memory

--- gcc/testsuite/gcc.c-torture/compile/20050622-1.c
+++ gcc/testsuite/gcc.c-torture/compile/20050622-1.c
@@ -1,8 +1,14 @@
-#if __SCHAR_MAX__ == 127 && __INT_MAX__ >= 2147483647
-struct S { char buf[72*1024*1024]; };
-#else
-struct S { char buf[64]; };
+/* { dg-additional-options "-DARRAY_SIZE=1024" { target { is_clang } } } */
+
+#ifndef ARRAY_SIZE
+  #if __SCHAR_MAX__ == 127 && __INT_MAX__ >= 2147483647
+    #define ARRAY_SIZE (72*1024*1024)
+  #else
+    #define ARRAY_SIZE 64
+  #endif
 #endif
+
+struct S { char buf[ARRAY_SIZE]; };
 
 extern void bar (struct S);
 
